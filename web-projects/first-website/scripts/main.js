55b340684c20ff6e90c9f7cec2a38c5b2cc64a27
anna mags <magarela@dickinson.edu>
2026-09-08 21:45:26 +0000

    edit button

--- web-projects/first-website/scripts/main.js
+++ web-projects/first-website/scripts/main.js
@@ -10,16 +10,21 @@ myImage.addEventListener('click', () => {
 });
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
-function setUserName() {
-  const myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = `Tomatoes are totally terrific, ${myName}`;
-}
+
 if (!localStorage.getItem('name')) {
   setUserName();
 } else {
   const storedName = localStorage.getItem('name');
   myHeading.textContent = `Tomatoes are totally terrific, ${storedName}`;
+}
+function setUserName() {
+  const myName = prompt('Please enter your name.');
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Tomatoes are totally terrific, ${myName}`;
+  }
 }
 myButton.addEventListener('click', () => {
   setUserName();
