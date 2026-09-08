0d7d537650ef614898a31eb95403617f661aba3a
anna mags <magarela@dickinson.edu>
2026-09-08 21:30:53 +0000

    added to change photo on click

--- web-projects/first-website/scripts/main.js
+++ web-projects/first-website/scripts/main.js
@@ -3,7 +3,7 @@ const myImage = document.querySelector('img');
 myImage.addEventListener('click', () => {
   const mySrc = myImage.getAttribute('src');
   if (mySrc === 'images/Tomato.png') {
-    myImage.setAttribute('src', 'images/Cherry_tomatoes.jpg');
+    myImage.setAttribute('src', 'images/Tomato2.png');
   } else {
     myImage.setAttribute('src', 'images/Tomato.png');
   }
