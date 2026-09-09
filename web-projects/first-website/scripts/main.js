4332fa347e0f53780141b17bdbb2ec852b231071
anna mags <magarela@dickinson.edu>
2026-09-09 01:11:18 +0000

    editting to see if their code works??

--- web-projects/first-website/scripts/main.js
+++ web-projects/first-website/scripts/main.js
@@ -14,14 +14,19 @@ let myHeading = document.querySelector('h1');
 
 function setUserName() {
   const myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = `Tomato is cool, ${myName}`;
-  if (!localStorage.getItem('name')) {
+  if (!myName) {
     setUserName();
   } else {
-    const storedName = localStorage.getItem('name');
-    myHeading.textContent = `Tomato is cool, ${storedName}`;
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
   }
+}
+
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem('name');
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
 }
 
 myButton.addEventListener('click', () => {
